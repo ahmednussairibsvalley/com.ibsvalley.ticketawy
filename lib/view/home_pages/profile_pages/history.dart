c22416497dbe794e43ab175d375b20b1d15012d9
Ahmed Nussair <ahmed.nussair@ibsvalley.com>
2019-09-15 20:20:57 +0200

    Initial Commit

--- lib/view/home_pages/profile_pages/history.dart
+++ lib/view/home_pages/profile_pages/history.dart
@@ -4,9 +4,39 @@ import 'package:ticketawy/view/home_pages/event_details_pages/dashed_divider.dar
 class ProfileHistory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return HistoryPage();
+    return HistorySlider();
   }
 }
+
+
+class HistorySlider extends StatefulWidget {
+  @override
+  _HistorySliderState createState() => _HistorySliderState();
+}
+
+class _HistorySliderState extends State<HistorySlider> with TickerProviderStateMixin{
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      vsync: this,
+      length: 3,
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return TabBarView(
+      controller: _tabController,
+      children: List.generate(_tabController.length, (index){
+        return HistoryPage();
+      }),
+    );
+  }
+}
+
 
 class HistoryPage extends StatelessWidget {
   @override
@@ -31,7 +61,6 @@ class HistoryPage extends StatelessWidget {
     );
   }
 }
-
 
 class HistoryItem extends StatelessWidget {
 
@@ -83,6 +112,58 @@ class HistoryItem extends StatelessWidget {
                   textAlign: TextAlign.center,
                   style: TextStyle(
                       fontSize: 18
+                  ),
+                ),
+              ),
+
+              // Ticket Type
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Color(0xfffe6700),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Image.asset('assets/ticket_type.png', width: 40, height: 40,),
+                        Text('Class A 120\$',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+
+              // Delete history
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Color(0xfffe6700),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Image.asset('assets/delete_history.png', width: 40, height: 40,),
+                        Text('Delete history',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
