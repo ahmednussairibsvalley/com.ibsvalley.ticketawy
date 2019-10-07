121b21b08de4151953543f27ad098181155353cd
Mahmoud-Moghazy <Mahmoud.ahmed@ibsvalley.com>
2019-10-07 13:22:16 +0200

    Initial Commit

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -45,7 +45,7 @@ class _LoginState extends State<Login> {
               Expanded(
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Text('Invalid login',
+                  child: Text(message.isEmpty?'Invalid login':message,
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       fontFamily: 'Verdana',
@@ -89,8 +89,8 @@ class _LoginState extends State<Login> {
     String _password = '';
     return Stack(
       children: <Widget>[
-        Globals.skipped?Scaffold(
-         body: Stack(
+        Scaffold(
+        body: Stack(
           children: <Widget>[
             // The background
             Container(
@@ -321,278 +321,45 @@ class _LoginState extends State<Login> {
             ): Container(),
           ],
         ),
-      )
-        :Scaffold(
-          body: Stack(
-            children: <Widget>[
-              // The background
-              Container(
-                height: _height,
-                width: _width,
-                decoration: BoxDecoration(
-                    color: Colors.deepPurple,
-                    image: DecorationImage(
-                        fit: BoxFit.fill,
-//                    colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.3), BlendMode.dstATop),
-                        image: AssetImage('assets/login.jpg'))),
-              ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.black,
+          child: GestureDetector(
+            onTap: () async {
+              if(_isUnderDevelopment){
+                _showUnderDevelopmentDialog(context);
+                return;
+              }
+              Globals.skipped = true;
+              Map userData = await getUserDetails();
 
-              Positioned(
-                left: 0.0,
-                right: 0.0,
-                top: _width > 360? _height / 11 : _height / 13,
-                bottom: 0.0,
-                child: GestureDetector(
-                  onTap: (){
-                    FocusScope.of(context).requestFocus(FocusNode());
-                  },
-                  child: Form(
-                    key: _formKey,
-                    child: ListView(
-                      children: <Widget>[
-                        Image.asset(
-                          'assets/header.png',
-                          width: 300,
-                          height: 195,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 20, bottom: 20, left: 50, right: 50,),
-                          child: DashedDivider(
-                            color: Colors.white30,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(
-                              left: 20, right: 20),
-                          child: Text('login',style: TextStyle(fontSize: 35,color: Colors.white),textAlign: TextAlign.center,),
-                        ),
+              Globals.controller.populateUser(userData);
+              List categoriesList = await categoryList();
 
-                        // User name.
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            left: 45.0,
-                            right: 45.0,
-                            bottom: 8.0,
-                            top: 20.0,
-                          ),
-                          child: TextFormField(
-                            keyboardType: TextInputType.number,
-                            decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Colors.white,
-                              prefixIcon: Icon(Icons.person_outline),
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(15),
-                                  borderSide: BorderSide(
-                                    style: BorderStyle.none,
-                                  )),
-                              labelStyle: TextStyle(
-                                fontSize: 15,
-                              ),
-                              hintText: 'Mobile Number',
-                            ),
-                            validator: (value) {
-                              if(value.isEmpty)
-                                return 'Please enter your mobile number';
-//                          else if(value != mobile)
-//                            return 'Wrong mobile number';
-                              _userName = value;
-                              return null;
-                            },
-                          ),
-                        ),
-
-                        // Password.
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            left: 45.0,
-                            right: 45.0,
-                            bottom: 8.0,
-                            top: 8.0,
-                          ),
-                          child: TextFormField(
-
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Colors.white,
-                              prefixIcon: Icon(Icons.lock_outline),
-//                          suffixIcon: Icon(Icons.help_outline),
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(15),
-                                  borderSide: BorderSide(
-                                    style: BorderStyle.none,
-                                  )),
-                              labelStyle: TextStyle(
-                                fontSize: 15,
-                              ),
-                              hintText: 'Password',
-                            ),
-                            validator: (value) {
-                              if(value.isEmpty)
-                                return 'Please enter your password';
-//                          else if(value != password)
-//                            return 'Wrong password';
-                              _password = value;
-                              return null;
-                            },
-                          ),
-                        ),
-
-                        // login button
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            left: 30.0,
-                            right: 30.0,
-                            top: 1.0,
-                          ),
-                          child: ListTile(
-                            onTap: () async {
-                              FocusScope.of(context).requestFocus(FocusNode());
-                              if(_formKey.currentState.validate()){
-                                setState(() {
-                                  _loggingIn = true;
-                                });
-
-                                Map response = await util.login(_userName, _password);
-
-                                if(response['result']){
-                                  Globals.userPassword = _password;
-                                  Globals.userId = response['id'];
-                                  Map userData = await getUserDetails();
-
-                                  Globals.controller.populateUser(userData);
-                                  List categoriesList = await categoryList();
-
-                                  Globals.controller.populateCategories(categoriesList);
-                                  Navigator.of(context).pushReplacementNamed('/home');
-                                } else{
-                                  setState(() {
-                                    _loggingIn = false;
-                                  });
-
-                                  _showLoginErrorDialog(context, message: response['user_Message']);
-                                }
-
-                              }
-                            },
-                            title: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.all(Radius.circular(10)),
-                                color: Color(0xfffe6700),
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(15.0),
-                                child: Text(
-                                  'login',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-
-                        // Sign up link text
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => Register()));
-                          },
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Padding(
-                                  padding: const EdgeInsets.all(0.0),
-                                  child: Column(
-                                    children: <Widget>[
-                                      Text(
-                                        'Don\'t have an account?',
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 18,
-                                        ),
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.only(right: 105, left: 105, top: 5),
-                                        child: DashedDivider(
-                                          width: 5,
-                                          color: Colors.white,
-                                        ),
-                                      )
-                                    ],
-                                  )
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
+              Globals.controller.populateCategories(categoriesList);
+              Navigator.of(context).pushReplacementNamed('/home');
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(15),
+                    color: Color(0xfffe6700),
+                    child: Text(
+                      'Skip This Step',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
                   ),
                 ),
-              ),
-
-              Globals.skipped?//The back arrow
-              Container(
-                alignment: Alignment.topLeft,
-                child: GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 40, bottom: 40 , left: 20, right: 40),
-                      child: Icon(
-                        Icons.arrow_back,
-                        color: Colors.white,
-                        size: 30,
-                      ),
-                    )),
-              ): Container(),
-            ],
-          ),
-          bottomNavigationBar: BottomAppBar(
-              color: Colors.black,
-              child: GestureDetector(
-                onTap: () async {
-                  if(_isUnderDevelopment){
-                    _showUnderDevelopmentDialog(context);
-                    return;
-                  }
-                  Globals.skipped = true;
-                  Map userData = await getUserDetails();
-
-                  Globals.controller.populateUser(userData);
-                  List categoriesList = await categoryList();
-
-                  Globals.controller.populateCategories(categoriesList);
-                  Navigator.of(context).pushReplacementNamed('/home');
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Expanded(
-                      child: Container(
-                        padding: EdgeInsets.all(15),
-                        color: Color(0xfffe6700),
-                        child: Text(
-                          'Skip This Step',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              ],
             ),
+          ),
         ),
+      ),
         _loggingIn? Positioned(
           top: 0.0, bottom: 0.0, left: 0.0, right: 0.0,
           child: Container(
